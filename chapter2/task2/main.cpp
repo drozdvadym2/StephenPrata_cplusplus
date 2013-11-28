d0d58dd3b8af4e003e5c49b4ba8c0831d02d0674
drozdvadym2 <drozdvadym@gmail.com>
2013-11-28 16:53:01 +0200

    Second task

---
+++ chapter2/task2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long distance;
+	cout << "Enter the distance in furlongs:" << endl;
+	cin >> distance;
+	cout << "Distance in yards is: " << 200 * distance << endl;
+
+	return 0;
+}
